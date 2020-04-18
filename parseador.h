cdc28681551a9581a55caa5314171726508be3d1
QuanticSausage <jonatan.dan@hotmail.com>
2020-04-18 19:47:43 -0300

    empeze el parser, no lo testie

--- parseador.h
+++ parseador.h
@@ -1,7 +1,23 @@
 #include <vector>
 #include <iostream>
+#include <string>
 
 typedef struct objeto{
 	unsigned int peso;
 	unsigned int resistencia;
 } objeto;
+//----------------------------------------------------------------------------------
+vector< objeto > parsearEntrada(){
+	string line;
+	getline(cin, line);
+	string cantidadObjetos;
+	string resistenciaTubo;
+	getline(line, cantidadObjetos, " ");
+	getline(line, resistenciaTubo, "\n");
+	for(;getline(cin, line);){
+		string pesoObjeto;
+		string resistenciaObjeto;
+		getline(line, pesoObjeto, " ");
+		getline(line, resistenciaObjeto, "\n");
+	}
+}
